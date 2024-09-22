271b05c275922cfbf908f2366c8da4449e60947c
mzubhan <msubhan2812@gmail.com>
2024-09-22 23:59:49 +0700

    Add delete function to delete all done tasks

--- script.js
+++ script.js
@@ -1,50 +1,59 @@
 const inputText = document.getElementById("task-todo");
-const inputDate = document.getElementById("date-field")
+const inputDate = document.getElementById("date-field");
+const inputPriority = document.getElementById("priority");
 const listContainer = document.getElementById("list-added");
+const listContainerDone = document.getElementById("list-field-done-wrapper")
+const deleteTaskButton = document.getElementById("deleteButton")
 
-function addTask() {
+function submit() {
     if (inputText.value === '') {
         alert ("Tugas tidak boleh kosong");
     }
+    else if (inputDate.value === '') {
+        alert ("Masukkan tanggal");
+    }
+    else if (inputPriority.value === '') {
+        alert ("Pilih prioritas tugas");
+    }
     else {
-      // Create the li element
       const li = document.createElement('li');
       li.classList.add("task-added")
 
-      // Create the checkbox input
       const checkbox = document.createElement('input');
       checkbox.type = 'checkbox';
       checkbox.classList.add('checkbox');
 
-      // Create the list-todo-value paragraph
       const todoValue = document.createElement('p');
       todoValue.classList.add('list-todo-value');
-      todoValue.textContent = inputText.value;  // Change as needed
+      todoValue.textContent = inputText.value; 
 
-      // Create the date-todo-value paragraph
       const dateValue = document.createElement('p');
       dateValue.classList.add('date-todo-value');
-      dateValue.textContent = inputDate.value ;  // Change as needed
+      dateValue.textContent = inputDate.value;
 
-      // Create the search-todo-value paragraph
       const searchValue = document.createElement('p');
       searchValue.classList.add('search-todo-value');
-      searchValue.textContent = 'Mahal!';  // Change as needed
+      searchValue.textContent = inputPriority.value
 
-      // Create the delete task button image
       const deleteButton = document.createElement('img');
       deleteButton.classList.add('delete-task-button');
       deleteButton.src = './asset/icons8-delete.svg';
       deleteButton.alt = 'Delete task';
 
-      // Append the elements to the li
       li.appendChild(checkbox);
       li.appendChild(todoValue);
       li.appendChild(dateValue);
       li.appendChild(searchValue);
       li.appendChild(deleteButton);
 
-      // Append the li to the ul (todo-list)
       document.getElementById('list-added').appendChild(li);
   }
 }
+
+// FUNGSI HAPUS SEMUA TUGAS
+deleteTaskButton.addEventListener('click', () => {
+    // Remove all ul elements inside the wrapper
+    const lists = listContainerDone.querySelectorAll('ul.list');
+    lists.forEach(list => list.remove());
+});
+
